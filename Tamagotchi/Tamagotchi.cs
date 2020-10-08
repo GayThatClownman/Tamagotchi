d27f5face19a84d56ea993ecde5066eeda6dd060
GayThatClownman <60339604+GayThatClownman@users.noreply.github.com>
2020-10-08 16:21:40 +0200

    Skapat klassen "Tamagotchi", variabler och metoder

---
+++ Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System;
+
+namespace Tamagotchi
+{
+    public class Tamagotchi
+    {
+        // Variabler.
+        private int hunger;
+        private int boredom;
+        private List<string> words;
+        private bool isAlive;
+        private Random generator;
+        public string name;
+
+        // Metoder
+        public void Feed()
+        {
+
+        }
+
+        public void Hi()
+        {
+
+        }
+
+        public void Teach(string word)
+        {
+
+        }
+
+        public void Tick()
+        {
+
+        }
+        public void PrintStats()
+        {
+
+        }
+        public bool GetAlive()
+        {
+            return isAlive;
+        }
+        private void ReduceBoredom()
+        {
+
+        }
+
+
+
+        
+
+    }
+
+}
